fd08da649f78b51d6fd3363077e87d327079d62e
Nisrina Arintia Maghfiroh <nisrinaarintia@gmail.com>
2017-10-03 23:25:47 +0700

    Update LinearListAsAbstractClass.java

--- src/no2sampai5/LinearListAsAbstractClass.java
+++ src/no2sampai5/LinearListAsAbstractClass.java
@@ -9,12 +9,13 @@ package no2sampai5;
  *
  * @author TOSHIBA
  */
-public interface LinearListAsAbstractClass {
- public abstract boolean isEmpty();
+public abstract class LinearListAsAbstractClass {
+    public abstract boolean isEmpty();
     public abstract int size();
     public abstract Object get(int index);
     public abstract int indexOf(Object elem);
     public abstract Object remove(int index);
-    public abstract void add(int indes, Object obj);
-    public abstract String toString();    
+    public abstract void add(int index, Object obj);
+    public abstract String toString();
+
 }
